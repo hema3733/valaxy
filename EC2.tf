70ead54492a8b61aad897913c3c5d00acc0cea2f
Ubuntu <ubuntu@ip-172-31-15-231.ap-south-1.compute.internal>
2024-08-20 07:00:36 +0000

    initial commit

---
+++ EC2.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+    region  = "ap-south-1"
+}
+resource "aws_instance" "demo_server"{
+    ami = "ami-02b49a24cfb95941c"
+    instance_type = "t2.micro"
+    key_name = "new-pair"
+}
